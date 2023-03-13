c547f7242dd226ad34ca083736337be1116a8581
Vadym Semcheno <vadym.semchenko@guesty.com>
2023-03-13 23:10:19 +0200

    server started

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+// import router from "./router.js";
+
+const PORT = 5001;
+
+const app = express();
+
+app.use(express.json());
+// app.use('/api', router)
+
+async function startApp() {
+    try {
+        app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+startApp();
